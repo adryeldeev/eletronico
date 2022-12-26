27117afc9884264af860b471aa50d24d2ff61cbd
Adryel martins <adryeldev@hotmail.com>
2022-12-26 12:54:27 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 5,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  }
+});
+
